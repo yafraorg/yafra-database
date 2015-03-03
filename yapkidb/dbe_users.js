7f372a40dcbeaacfaff8ce3e694142315c33c75d
yafraorg <yafraorg@gmail.com>
2015-03-03 20:53:53 +0100

    change to 2.6 mongo syntax

--- yapkidb/dbe_users.js
+++ yapkidb/dbe_users.js
@@ -1,6 +1,13 @@
 use yapki
-db.addUser( 
+db.createUser( 
  {
     user: "yafraadmin",
     pwd: "yafra",
     roles: [ { role: "userAdmin", db: "yapki" } ]   } )
+
+#use admin
+#db.createUser( 
+# {
+#    user: "admin",
+#    pwd: "yafra",
+#    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]   } )
