2859358a0c8372203ba105336bdebacef5621889
Yafra <yafraorg@gmail.com>
2015-03-05 20:38:36 +0100

    fixed mongo script - runs now

--- yapkidb/dbe_users.js
+++ yapkidb/dbe_users.js
@@ -1,13 +1,5 @@
-use yapki
 db.createUser( 
  {
     user: "yafraadmin",
     pwd: "yafra",
     roles: [ { role: "userAdmin", db: "yapki" } ]   } )
-
-#use admin
-#db.createUser( 
-# {
-#    user: "admin",
-#    pwd: "yafra",
-#    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]   } )
